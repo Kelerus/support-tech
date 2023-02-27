b092229c2e930c7f139eb034a4a18ef1fdb0b14c
Kelerus <11herobrineplay@mail.ru>
2023-02-27 22:59:26 +0400

    models tickets, cities, categories, messages

---
+++ database/migrations/2023_02_27_175922_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Ticket::class, 'ticket')->constrained()->nullable(false);
+            $table->foreignIdFor(User::class, 'user')->constrained()->nullable(false);
+            $table->string('message', 2048);
+            $table->json('files');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
